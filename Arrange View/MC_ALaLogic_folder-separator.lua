8a00d725bd0cccb8ab21b6db4acd60b5bc0c93a1
MathieuCGit <83604945+MathieuCGit@users.noreply.github.com>
2021-11-21 00:29:33 +0100

    a bit of code sanitize (comments, indent,etc.)

--- Arrange View/MC_ALaLogic_folder-separator.lua
+++ Arrange View/MC_ALaLogic_folder-separator.lua
@@ -1,13 +1,14 @@
 --[[
    * @description Draw separator on folder track - aka A la Logic X
-   * Lua script for Cockos REAPER
-   * @author: Mathieu CONAN
+   * @author: Mathieu CONAN   
+   * @version 0.1
+   * @about 
+   *	- This script aims to reproduce the folder separation in a way Logic X does it
+   * 	- User URI: https://forum.cockos.com/member.php?u=153781   
+   *
    * Licence: GPL v3
    * REAPER: 6.0
-   * @version 0.1
    * Extensions: None 
-   * @about This script aims to reproduce the folder separation in a way Logic X does it
-   *  User URI: https://forum.cockos.com/member.php?u=153781   
 --]]
 
 --[[
@@ -35,101 +36,104 @@ TRACK_COLOR={111,121,131} -- use RGB color code. Default is {111,121,131}
 
 TRACK_COLOR_DARKER_STEP = 25 --this is the amount of darkness yo uwant to apply to default track color. 0 means NO darkness. Default is 25
 
-TRACK_LOCK=1 -- 1 means track height is locked, 0 means not locked. Default is 1
+ITEM_LOCK=1 -- 1 means items is locked and can't be moved/cut/split/nothing. But actually Reaper allows to many thing to happen to locked item so this option is for future versions. Default is 1
 
 
 --
 --[[ Various Functions]]
 --
 
-  ---Debug function - display messages in reaper console
-  --@tparam string String aims to be displayed in the reaper console
-  function Debug(String)
-    reaper.ShowConsoleMsg(tostring(String).."\n")
-  end
+	---Debug function - display messages in reaper console
+	--@tparam string String aims to be displayed in the reaper console
+	function Debug(String)
+		reaper.ShowConsoleMsg(tostring(String).."\n")
+	end
 
-  ---create item on track passd in argument
-  function createLogicXItem (track)
-      local lastItemTimeEnd=0
-      lastItemTimeEnd=getLastItemTimeEnd()
-      _,trackName = reaper.GetSetMediaTrackInfo_String( track, "P_NAME",0,0)-- get track name and 
-      startTime, endTime = reaper.BR_GetArrangeView(0) --project start and end
+	---create item on track passd in argument
+	-- @tparam track track a reaper track ressource
+	function createLogicXItem (track)
+		local lastItemTimeEnd=0
+		lastItemTimeEnd=getLastItemTimeEnd()
+		_,trackName = reaper.GetSetMediaTrackInfo_String( track, "P_NAME",0,0)-- get track name and 
+		startTime, endTime = reaper.BR_GetArrangeView(0) --project start and end
 
-      if endTime > lastItemTimeEnd+100 then
-      -- if zoom out is too important, some actions like "View: Zoom out project"
-      -- fails to adjust size and you have to zoom manually with mouse to get it right
-      -- so for now we put a limit based on the lastest item time end + 100...welll...100 is arbitrary...
-        endTime=lastItemTimeEnd+50
-        endTime=math.floor(endTime+0.5)--we need an integer so we round the float
-        startTime=math.floor(startTime+0.5)
-        _, _ = reaper.GetSet_ArrangeView2( 0, 1, startTime, endTime )
-      end
+		if endTime > lastItemTimeEnd+100 then
+			-- if zoom out is too important, some actions like "View: Zoom out project"
+			-- fails to adjust size and you have to zoom manually with mouse to get it right
+			-- so for now we put a limit based on the lastest item time end + 100...welll...100 is arbitrary...
+			endTime=lastItemTimeEnd+50
+			endTime=math.floor(endTime+0.5)--we need an integer so we round the float
+			startTime=math.floor(startTime+0.5)
+			_, _ = reaper.GetSet_ArrangeView2( 0, 1, startTime, endTime )
+		end
 
-      -- and create an item which is project lenght
-      reaper.AddMediaItemToTrack(track)
-      item = reaper.GetTrackMediaItem(track,0)
-      reaper.SetMediaItemInfo_Value(item, "D_LENGTH", endTime)
-      
-      --we need at least one active take to get the track name as a label on items
-      nbrOfTake = reaper.CountTakes( item )
-      if nbrOfTake == 0 then
-      --So if we have no take, we create one
-        reaper.AddTakeToMediaItem( item )--add a new take
-        take = reaper.GetActiveTake(item) -- make it active   
-        reaper.GetSetMediaItemTakeInfo_String(take, "P_NAME", trackName, 1); --rename active take into track name
-      end
-      
-      -- Reaper doesn't prevent an item of being splited even if the item is locked
-      -- so this option isn't really useful for now but should be in the future
-      -- it only offers a darker color so for now we'd rather to disable it
-      -- reaper.SetMediaItemInfo_Value( item, "C_LOCK", TRACK_LOCK )
-      
-      -- If we want one background color for every items instead of defaut folder color
-      if TRACK_COLOR_SINGLE == 1 then
-        color=reaper.ColorToNative(TRACK_COLOR[1],TRACK_COLOR[2],TRACK_COLOR[3])|0x1000000
-        reaper.SetMediaItemInfo_Value(item, "I_CUSTOMCOLOR", color)
-      end
-      
-      -- If we want one background color for every items instead of defaut folder color
-      if TRACK_COLOR_DARKER_STEP > 0 and TRACK_COLOR_SINGLE == 0 then
-        intTrackColor=reaper.GetMediaTrackInfo_Value( track, "I_CUSTOMCOLOR" )
-        red, green, blue = reaper.ColorFromNative( intTrackColor )
-        -- Debug("red : "..red.." green: "..green.." blue : "..blue)
-        
-        R=red - TRACK_COLOR_DARKER_STEP 
-        if R < 0 then R =255 - R end
-        G=green - TRACK_COLOR_DARKER_STEP 
-        if G < 0 then G =255 - G end
-        B=blue - TRACK_COLOR_DARKER_STEP 
-        if B < 0 then B =255 - B end
-        
-        -- Debug("R : "..R.." G: "..G.." B : "..B)
-        color=reaper.ColorToNative(R,G,B)|0x1000000
-        reaper.SetMediaItemInfo_Value(item, "I_CUSTOMCOLOR", color)
-      end    
-      
-      -- we set track height and lock the track height
-      reaper.SetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE", TRACK_HEIGHT);
-      reaper.SetMediaTrackInfo_Value(track, "B_HEIGHTLOCK",1)
-  end
+		-- and create an item which is project lenght
+		reaper.AddMediaItemToTrack(track)
+		item = reaper.GetTrackMediaItem(track,0)
+		reaper.SetMediaItemInfo_Value(item, "D_LENGTH", endTime)
+
+		--we need at least one active take to get the track name as a label on items
+		nbrOfTake = reaper.CountTakes( item )
+		if nbrOfTake == 0 then
+		--So if we have no take, we create one
+			reaper.AddTakeToMediaItem( item )--add a new take
+			take = reaper.GetActiveTake(item) -- make it active   
+			reaper.GetSetMediaItemTakeInfo_String(take, "P_NAME", trackName, 1); --rename active take into track name
+		end
+
+		-- Reaper doesn't prevent an item of being splited even if the item is locked
+		-- so this option isn't really useful for now but should be in the future
+		-- it only offers a darker color so for now we'd rather to disable it
+		-- reaper.SetMediaItemInfo_Value( item, "C_LOCK", ITEM_LOCK )
+
+		if TRACK_COLOR_SINGLE == 1 then
+		-- If we want one background color for every items instead of defaut folder color
+			color=reaper.ColorToNative(TRACK_COLOR[1],TRACK_COLOR[2],TRACK_COLOR[3])|0x1000000
+			reaper.SetMediaItemInfo_Value(item, "I_CUSTOMCOLOR", color)
+		end
+
+		if TRACK_COLOR_DARKER_STEP > 0 and TRACK_COLOR_SINGLE == 0 then
+		-- If we want one background color for every items instead of defaut folder color
+		
+			intTrackColor=reaper.GetMediaTrackInfo_Value( track, "I_CUSTOMCOLOR" )
+			red, green, blue = reaper.ColorFromNative( intTrackColor )
+			-- Debug("red : "..red.." green: "..green.." blue : "..blue)
+
+			R=red - TRACK_COLOR_DARKER_STEP 
+			if R < 0 then R =255 - R end
+			G=green - TRACK_COLOR_DARKER_STEP 
+			if G < 0 then G =255 - G end
+			B=blue - TRACK_COLOR_DARKER_STEP 
+			if B < 0 then B =255 - B end
+
+			-- Debug("R : "..R.." G: "..G.." B : "..B)
+			color=reaper.ColorToNative(R,G,B)|0x1000000
+			reaper.SetMediaItemInfo_Value(item, "I_CUSTOMCOLOR", color)
+		end    
+
+		-- we set track height and lock the track height
+		reaper.SetMediaTrackInfo_Value(track, "I_HEIGHTOVERRIDE", TRACK_HEIGHT);
+		reaper.SetMediaTrackInfo_Value(track, "B_HEIGHTLOCK",1)
+	end
   
-    ---remove all items from arrange view. This function is called with reaper.atexit()
-  function cleanArrangeView()
-    nbrOfTrack =  reaper.CountTracks(0)--nbre of track in the project
+	---remove all items from arrange view. This function is called with reaper.atexit()
+	function cleanArrangeView()
+		nbrOfTrack =  reaper.CountTracks(0)--nbre of track in the project
+
+		for i=0, nbrOfTrack-1 do
+		-- for each track
+		track =  reaper.GetTrack( 0, i ) --we get info from the current track
+		trackFolderDepth = reaper.GetMediaTrackInfo_Value( track, "I_FOLDERDEPTH") --we check if it's a folder or not
+
+			if trackFolderDepth > 0.0 then -- if we are on a folder track
+			reaper.SetMediaTrackInfo_Value(track, "B_HEIGHTLOCK",0)--unlock track height
+			deleteEmptyItemsOnTracks(track)
+			end
+		end
+	end
   
-    for i=0, nbrOfTrack-1 do
-    -- for each track
-      track =  reaper.GetTrack( 0, i ) --we get info from the current track
-      trackFolderDepth = reaper.GetMediaTrackInfo_Value( track, "I_FOLDERDEPTH") --we check if it's a folder or not
-      
-      if trackFolderDepth > 0.0 then -- if we are on a folder track
-        reaper.SetMediaTrackInfo_Value(track, "B_HEIGHTLOCK",0)--unlock track height
-        deleteEmptyItemsOnTracks(track)
-      end
-    end
-  end
-  
-  --- Delete empty item on the track passed
+	--- Delete empty item on the track passed
+	-- @tparam track track a reaper track ressource
     function deleteEmptyItemsOnTracks(track)
         local nbrOfItems= reaper.GetTrackNumMediaItems(track)--get nbre of items on this track
         
@@ -144,87 +148,82 @@ TRACK_LOCK=1 -- 1 means track height is locked, 0 means not locked. Default is 1
         
     end
   
-  --- check if an item located on folder track contains MIDI data
-  --@tparam item item a Reaper media item
-  function doesItemContainsMidiData(item)
-    if item ~= nil then --if there is an item
-    take=reaper.GetActiveTake(item) -- we get the active take from it
-      if take ~= nil then --if there is an active take
-      containsMidi = reaper.TakeIsMIDI( take ) -- we check if it contains MIDI data
-      --this check is very impotant to prevent removing already existing items with content onto folder tracks
-        if containsMidi == false then
-          return false
-        else
-          return true
-        end
-      end
-    end
-  end
+	--- check if an item located on folder track contains MIDI data
+	--@tparam item item a Reaper media item
+	function doesItemContainsMidiData(item)
+		if item ~= nil then --if there is an item
+		take=reaper.GetActiveTake(item) -- we get the active take from it
+			if take ~= nil then --if there is an active take
+				containsMidi = reaper.TakeIsMIDI( take ) -- we check if it contains MIDI data
+				--this check is very impotant to prevent removing already existing items with content onto folder tracks
+				if containsMidi == false then
+					return false
+				else
+					return true
+				end
+			end
+		end
+	end
   
-  ---We get the end of the lastest item in the project
-    --@tparam track track is a Reaper Track
-    --
+	---We get the end of the lastest item in the project
     --@treturn int lastItemTimeEnd is the time in second of the end of the last item on the timeline
-  function getLastItemTimeEnd()
-    local lastItemTimeEnd=0
-        nbrOfTrack =  reaper.CountTracks(0)--nbre of track in the project
-    
-    for i=0, nbrOfTrack-1 do
-    --for each track
-      track =  reaper.GetTrack( 0, i ) --we get info from the current track
-      trackFolderDepth = reaper.GetMediaTrackInfo_Value( track, "I_FOLDERDEPTH") --we check if it's a folder or not
-      
-    if trackFolderDepth <= 0.0 then 
-    --if track is NOT a folder (normal=0) or the last track of a folder (negative values)
-    nbrOfItems= reaper.GetTrackNumMediaItems(track)--get nbre of items on this track
-      
-      for j=0, nbrOfItems-1 do
-      --for each item 
-        item =  reaper.GetTrackMediaItem( track, j )--we get current item info
-        itemStart = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
-        itemLen = reaper.GetMediaItemInfo_Value( item, "D_LENGTH" )
-        itemEnd = itemStart+itemLen
-        
-        if itemEnd > lastItemTimeEnd then
-        --if the selected item ends later than the previous, we use this end time as new project end time
-        lastItemTimeEnd = itemEnd
-        end
+	function getLastItemTimeEnd()
+		local lastItemTimeEnd=0
+		nbrOfTrack =  reaper.CountTracks(0)--nbre of track in the project
 
-      end
-    end
-    end
-  -- reaper.SetEditCurPos( lastItemTimeEnd, 1, 0 )
-  -- Debug( reaper.GetCursorPosition())  
-  return lastItemTimeEnd
-  end
+		for i=0, nbrOfTrack-1 do
+		--for each track
+			track =  reaper.GetTrack( 0, i ) --we get info from the current track
+			trackFolderDepth = reaper.GetMediaTrackInfo_Value( track, "I_FOLDERDEPTH") --we check if it's a folder or not
 
-  --- Allow us to make the script toggled (on/off) in the action list. This way it can be persistant at reaper satartup
-  -- this function is a total and unshamed copy/paste from awesome Lokasenna - Track selection follows item selection
-  -- https://raw.githubusercontent.com/ReaTeam/ReaScripts/master/Items Properties/Lokasenna_Track selection follows item selection.lua
-  (function()
-  local _, _, sectionId, cmdId = reaper.get_action_context()
+			if trackFolderDepth <= 0.0 then 
+			--if track is NOT a folder (normal=0) or the last track of a folder (negative values)
+			nbrOfItems= reaper.GetTrackNumMediaItems(track)--get nbre of items on this track
 
-    if sectionId ~= -1 then
-    --if script is running
-      cleanArrangeView()--clean folder track from every empty items except those with MIDI data
-      reaper.SetToggleCommandState(sectionId, cmdId, 1)--set toggle state to On in action list
-      reaper.RefreshToolbar2(sectionId, cmdId) --set toggle State to On in toolbar
+				for j=0, nbrOfItems-1 do
+				--for each item 
+				item =  reaper.GetTrackMediaItem( track, j )--we get current item info
+				itemStart = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
+				itemLen = reaper.GetMediaItemInfo_Value( item, "D_LENGTH" )
+				itemEnd = itemStart+itemLen
 
-      reaper.atexit(function()
-      --before script totaly stop
-        reaper.SetToggleCommandState(sectionId, cmdId, 0) --set toggle state to Off in action list
-        reaper.RefreshToolbar2(sectionId, cmdId)--set toggle State to Off in toolbar
-        cleanArrangeView()--clean folder track from every empty items except those with MIDI data
-      end)
-    end
-  end)()
+					if itemEnd > lastItemTimeEnd then
+					--if the selected item ends later than the previous, we use this end time as new project end time
+					lastItemTimeEnd = itemEnd
+					end
+
+				end
+			end
+		end
+	return lastItemTimeEnd
+	end
+
+	--- Allow us to make the script toggled (on/off) in the action list. This way it can be persistant at reaper satartup
+	-- this function is a total and unshamed copy/paste from awesome Lokasenna - Track selection follows item selection
+	-- https://raw.githubusercontent.com/ReaTeam/ReaScripts/master/Items Properties/Lokasenna_Track selection follows item selection.lua
+	(function()
+		local _, _, sectionId, cmdId = reaper.get_action_context()
+
+		if sectionId ~= -1 then
+			--if script is running
+			cleanArrangeView()--clean folder track from every empty items except those with MIDI data
+			reaper.SetToggleCommandState(sectionId, cmdId, 1)--set toggle state to On in action list
+			reaper.RefreshToolbar2(sectionId, cmdId) --set toggle State to On in toolbar
+
+				reaper.atexit(function()
+				--before script totaly stop
+				reaper.SetToggleCommandState(sectionId, cmdId, 0) --set toggle state to Off in action list
+				reaper.RefreshToolbar2(sectionId, cmdId)--set toggle State to Off in toolbar
+				cleanArrangeView()--clean folder track from every empty items except those with MIDI data
+				end)
+		end
+	end)()
     
 --
 --[[ CORE ]]--
 --
 function Main()
-  
-  nbrOfTrack =  reaper.CountTracks(0)--nbre of track in the project
+	nbrOfTrack =  reaper.CountTracks(0)--nbre of track in the project
   
     for i=0, nbrOfTrack-1 do
     -- for each track
@@ -232,7 +231,7 @@ function Main()
     trackFolderDepth = reaper.GetMediaTrackInfo_Value( track, "I_FOLDERDEPTH") --we check if it's a folder or not
 
         if trackFolderDepth > 0.0 then -- if we are on a folder track
-      deleteEmptyItemsOnTracks(track)--we clean the tracks from empty items    
+			deleteEmptyItemsOnTracks(track)--we clean the tracks from empty items    
             createLogicXItem(track)-- Once track is cleared from empty items but still has items with MIDI data, we create an empty item
         end
     end
