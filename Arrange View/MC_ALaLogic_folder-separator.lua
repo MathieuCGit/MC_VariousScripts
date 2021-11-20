cf66afac3e1d8f168ca7edb96cca98a103591a9e
MathieuCGit <83604945+MathieuCGit@users.noreply.github.com>
2021-11-21 00:35:59 +0100

    Reapack : minor about issue

--- Arrange View/MC_ALaLogic_folder-separator.lua
+++ Arrange View/MC_ALaLogic_folder-separator.lua
@@ -2,9 +2,8 @@
    * @description Draw separator on folder track - aka A la Logic X
    * @author: Mathieu CONAN   
    * @version 0.1
-   * @about 
-   *	- This script aims to reproduce the folder separation in a way Logic X does it
-   * 	- User URI: https://forum.cockos.com/member.php?u=153781   
+   * @about This script aims to reproduce the folder separation in a way Logic X does it
+   * 	User URI: https://forum.cockos.com/member.php?u=153781   
    *
    * Licence: GPL v3
    * REAPER: 6.0
